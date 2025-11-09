26813c30db7a496952d81a19b1f85d30279c8dcb
xiaobainya <jj7375350@gmail.com>
2025-11-09 20:49:09 +0800

    code

--- 2x.cpp
+++ 2x.cpp
@@ -2,14 +2,21 @@
 #include <iostream>
 // 求一元二次方程
 int main() {
-  float a, b, c, x1, x2;
+  float a, b, c, x1, x2, x;
   std::cin >> a >> b >> c;
-  if (b * b >= 4 * a * c) {
-    x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
-    x2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
-    std::cout << "x1=" << x1;
-    std::cout << "x2=" << x2;
+  if (a == 0 && b != 0) {
+    x = -c / b;
+    std::cout << x;
+  } else if (a != 0) {
+    if (b * b >= 4 * a * c) {
+      x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+      x2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
+      std::cout << "x1=" << x1 << std::endl;
+      std::cout << "x2=" << x2 << std::endl;
+    } else {
+      std::cout << "error";
+    }
   } else {
-    return 0;
+    std::cout << "error";
   }
 }
