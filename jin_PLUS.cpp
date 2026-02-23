157c7de0e4b3c9016853e0635c0778fb7c13c32a
xiaobainya <jj7375350@gmail.com>
2026-02-23 19:11:14 +0800

    change

--- jin_PLUS.cpp
+++ jin_PLUS.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-int main() {
-  std::cout << "输入层数：";
-  unsigned int a, b = 1;
-  std::cin >> a;
+#include <string>
+void pt(int a) {
+  if (a <= 0) {
+    throw std::string("传入值a<=0");
+  }
   for (int i = 1; i <= a; i++) {
     for (int j = i; j < a; j++) {
       std::cout << " ";
@@ -11,5 +12,15 @@ int main() {
       std::cout << "* ";
     }
     std::cout << std::endl;
+  }
+}
+int main() {
+  std::cout << "输入层数：";
+  int num;
+  std::cin >> num;
+  try {
+    pt(num);
+  } catch (const std::string &text) {
+    std::cout << "ERROR：" << text << std::endl;
   }
 }
